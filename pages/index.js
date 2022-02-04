a7770fa76c18ae49dc164bca3bf0da7bce6403e0
RaHuLMeghani24 <rahulmeghani36@gmail.com>
2022-02-04 18:01:32 -0500

    Setting up the project

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import Sidebar from '../components/Sidebar';
+
+export default function Home() {
+  return (
+    <div className=''>
+  
+
+    <main>
+      <Sidebar/>
+      {/* Center */}
+    </main>
+
+
+      <div>
+      {/* Player */}
+      </div>
+    </div>
+  )
+}

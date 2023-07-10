11c5e280c176b943cea8192ceff39289f2fac423
RaHuLMeghani24 <rahulmeghani36@gmail.com>
2023-07-09 21:13:08 -0400

    Bug Fixed: removed next-auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,13 @@
-import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/spotify";
+// import NextAuth from "next-auth";
+// import GithubProvider from "next-auth/providers/spotify";
 
-export default NextAuth({
-  // Configure one or more authentication providers
-  providers: [
-    SpotifyProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    // ...add more providers here
-  ],
-});
+// export default NextAuth({
+//   // Configure one or more authentication providers
+//   providers: [
+//     SpotifyProvider({
+//       clientId: process.env.GITHUB_ID,
+//       clientSecret: process.env.GITHUB_SECRET,
+//     }),
+//     // ...add more providers here
+//   ],
+// });
